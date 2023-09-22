ef3c3a13eca0af3f2bbd4d8f5299f9a076ee6863
Bagavathi <sbagavathi2006@gmail.com>
2023-09-21 20:31:34 -0500

    Completed Controller Studio

--- SpringBootNew/skills-tracker/skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ SpringBootNew/skills-tracker/skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -52,7 +52,6 @@ public class SkillsController {
                 "</body>" +
                 "</html>";
         return Initial+"\n\n"+html;
-
     }
     @RequestMapping("submit")
     @ResponseBody
